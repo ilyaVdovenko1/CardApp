a32ed260bd6f5ec94a401079b1032177b599e413
ilyaVdovenko1 <ilyaforstudy1@gmail.com>
2022-09-08 13:11:07 +0300

    Added basic design.

---
+++ CardsApp.Client/CardsApp.Client.Wpf/Components/CardsList.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace CardsApp.Client.Wpf.Components;
+
+public partial class CardsList : UserControl
+{
+    public CardsList()
+    {
+        InitializeComponent();
+    }
+}

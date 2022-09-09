dae6d228b93ed1eefe29efac38b8831fe3834182
ilyaVdovenko1 <ilyaforstudy1@gmail.com>
2022-09-09 07:33:04 +0300

    Added domain functionality.

---
+++ CardsApp.Client/CardsApp.Client.Domain/Interfaces/ICardsAppService.cs
@@ -0,0 +1,16 @@
+﻿using CardsApp.Client.Domain.Models;
+
+namespace CardsApp.Client.Domain.Interfaces;
+
+public interface ICardsAppService
+{
+    public Task CreateCard(Card card);
+    
+    public Task<IEnumerable<Card>> GetAllCards();
+
+    public Task<Card> GetCardById(Guid id);
+
+    public Task UpdateCard(Guid id, Card card);
+
+    public Task DeleteCard(Guid id);
+}
---
+++ CardsApp.Client/CardsApp.Client.Domain/Services/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿
+using CardsApp.Client.ApiConnection.Services;
+using CardsApp.Client.Domain.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CardsApp.Client.Domain.Services;
+
+public static class DependencyInjection
+{
+    public static ICardsAppService AddCardsAppService(this ServiceCollection serviceCollection, IConfiguration configuration)
+    {
+        
+        var apiCardService = serviceCollection.AddApiCardService(configuration);
+        serviceCollection.AddSingleton<ICardsAppService>(new CardsAppService(apiCardService));
+
+        return serviceCollection.BuildServiceProvider().GetService<ICardsAppService>()!;
+
+    }
+}

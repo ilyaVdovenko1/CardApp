d900d240589d63e0c0437804806d781ad376fa42
ilyaVdovenko1 <ilyaforstudy1@gmail.com>
2022-09-09 07:33:28 +0300

    Added delete command.

---
+++ CardsApp.Client/CardsApp.Client.Wpf/Commands/DeleteCardCommand.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using CardsApp.Client.Wpf.Stores;
+using CardsApp.Client.Wpf.ViewModels;
+
+namespace CardsApp.Client.Wpf.Commands;
+
+public class DeleteCardCommand : AsyncCommandBase
+{
+    private readonly CardsListItemViewModel cardsListItemViewModel;
+    private readonly CardsStore cardsStore;
+
+
+    public DeleteCardCommand(CardsListItemViewModel cardsListItemViewModel, CardsStore cardsStore)
+    {
+        this.cardsListItemViewModel = cardsListItemViewModel;
+        this.cardsStore = cardsStore;
+    }
+
+    public override async Task ExecuteAsync(object? parameter)
+    {
+        try
+        {
+            var cardToDelete = this.cardsListItemViewModel.CardModel;
+            await this.cardsStore.Delete(cardToDelete.Id);
+        }
+        catch
+        {
+            throw;
+        }
+        
+    }
+}
